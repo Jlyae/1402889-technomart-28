86b1e1473a8842a07b02c99808de7eee93a7265c
Jlyae <JlyaE@ngs.ru>
2020-07-10 18:48:04 +0700

    Итоговые доработки

--- js/popup.js
+++ js/popup.js
@@ -8,6 +8,11 @@ var map = document.querySelector(".maps-popup");
 var mapJpg = document.querySelector(".maps-little");
 var closeMap = document.querySelector(".button-close-map");
 
+var addBasket = document.querySelectorAll(".buy");
+var windowBasket = document.querySelector(".basket-ok");
+var closeBasket = document.querySelector(".button-close-basket");
+var elementBasket = "";
+
 var isStorageSupport = true;
 var storage = "";
 
@@ -17,58 +22,88 @@ try {
     isStorageSupport = false;
 }
 
-writeUs.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    letter.classList.add("letter-show");
-    if (storage) {
-        letterName.value = storage;
-        letterEmail.focus();
-    }
-    else {
-        letterName.focus();
-    }
-});
-
-letterClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    letter.classList.remove("letter-show");
-    letter.classList.remove("modal-error");
-});
-
-letterForm.addEventListener("submit", function (evt) {
-    if (!letterName.value || !letterEmail.value) {
+if (writeUs) {
+    writeUs.addEventListener("click", function (evt) {
         evt.preventDefault();
-        letter.classList.remove("modal-error");
-        letter.offsetWidth = letter.offsetWidth;
-        letter.classList.add("modal-error");
-    }
-    else {
-        if (isStorageSupport) {
-            localStorage.setItem("letterName", letterName.value);
+        letter.classList.add("letter-show");
+        if (storage) {
+            letterName.value = storage;
+            letterEmail.focus();
         }
-    }
-});
+        else {
+            letterName.focus();
+        }
+    });
+}
+
+if (letterClose) {
+    letterClose.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        letter.classList.remove("letter-show");
+        letter.classList.remove("modal-error");
+    });
+}
+
+if (letterForm) {
+    letterForm.addEventListener("submit", function (evt) {
+        if (!letterName.value || !letterEmail.value) {
+            evt.preventDefault();
+            letter.classList.remove("modal-error");
+            letter.offsetWidth = letter.offsetWidth;
+            letter.classList.add("modal-error");
+        }
+        else {
+            if (isStorageSupport) {
+                localStorage.setItem("letterName", letterName.value);
+            }
+        }
+    });
+}
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-        if (letter.classList.contains("letter-show")) {
+        if ((letter) && (letter.classList.contains("letter-show"))) {
             evt.preventDefault();
             letter.classList.remove("letter-show");
             letter.classList.remove("modal-error");
         }
-        else if (map.classList.contains("maps-show")) {
+        else if ((map) && (map.classList.contains("maps-show"))) {
             evt.preventDefault();
             map.classList.remove("maps-show");
+        }
+        else if ((windowBasket) && (windowBasket.classList.contains("basket-show"))) {
+            evt.preventDefault();
+            windowBasket.classList.remove("basket-show");
         }
     }
 });
 
-mapJpg.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    map.classList.add("maps-show");
-});
+if (mapJpg) {
+    mapJpg.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        map.classList.add("maps-show");
+    });
+}
 
-closeMap.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    map.classList.remove("maps-show");
-});
+if (closeMap) {
+    closeMap.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        map.classList.remove("maps-show");
+    });
+}
+
+if (addBasket) {
+    for (elementBasket of addBasket) {
+        elementBasket.addEventListener("click", function (evt) {
+            evt.preventDefault();
+            windowBasket.classList.add("basket-show");
+        });
+    }
+}
+
+if (closeBasket) {
+    closeBasket.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        windowBasket.classList.remove("basket-show");
+    });
+}
